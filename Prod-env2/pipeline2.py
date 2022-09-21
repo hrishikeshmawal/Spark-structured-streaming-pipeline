e9db5aa90a38acc1c9197b734ae9c341aa07e018
Hrishikesh Mawal <82875031+hrishikeshmawal@users.noreply.github.com>
2022-09-21 16:16:19 +0200

    Update pipeline2.py

--- Prod-env2/pipeline2.py
+++ Prod-env2/pipeline2.py
@@ -1,7 +1,6 @@
 import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
-from pyspark.sql.types import *
 from pyspark.sql.functions import *
 from pyspark.sql import functions as F
 import os
